0ba17d769f513b2a525735cf1e5f66d4a885676c
Alessio Gottardo <tappoz84@yahoo.it>
2018-02-13 10:43:38 +0000

    Control the type of browser with an extra parameter

--- open_browser_tabs.py
+++ open_browser_tabs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+print("\n\nUsage: `./open_browser_tabs.py open_browser_tabs.csv [firefox|chromium]`\n\n")
+
 # INFO
 # tested with Python 3.5.3
 
@@ -7,8 +9,7 @@ import os
 import sys
 import subprocess
 
-# TODO choose browser with command line parameter
-chromium_cmd_template = "chromium --incognito {} &"
+chromium_cmd_template = "chromium --incognito --new-window {} &"
 firefox_cmd_template = "firefox -private {} &"
 
 def load_urls(urls_path):
@@ -17,16 +18,27 @@ def load_urls(urls_path):
     urls = [x.strip().replace('\n', '') for x in urls]
   return urls
 
-def open_tabs(urls):
-  chromium_urls_str = ''
+def __build_firefox_cmd(urls):
   firefox_urls_str = ''
   for idx, url in enumerate(urls):
-    chromium_urls_str = chromium_urls_str + ' ' + url
     firefox_urls_str = firefox_urls_str + "-new-tab -url {} ".format(url)
-  chromium_browser_cmd = chromium_cmd_template.format(chromium_urls_str)
-  firefox_browser_cmd = firefox_cmd_template.format(firefox_urls_str)
+  return firefox_cmd_template.format(firefox_urls_str)
+
+def __build_chrome_cmd(urls):
+  chromium_urls_str = ''
+  for idx, url in enumerate(urls):
+    chromium_urls_str = chromium_urls_str + ' ' + url
+  return chromium_cmd_template.format(chromium_urls_str)
+
+def open_tabs(urls, browser_name):
+  if browser_name == 'firefox':
+    browser_cmd = __build_firefox_cmd(urls)
+  elif browser_name == 'chromium':
+    browser_cmd = __build_chrome_cmd(urls)
+  else:
+    raise ValueError("Can not recognize this browser parameter: '{}'", browser_name)
   try:
-    execution_stats = subprocess.run(firefox_browser_cmd, shell=True, check=True)
+    execution_stats = subprocess.run(browser_cmd, shell=True, check=True)
     print(execution_stats)
   except subprocess.CalledProcessError as e:
     sys.exit(1)
@@ -34,4 +46,5 @@ def open_tabs(urls):
 if __name__ == '__main__':
   urls_path = sys.argv[1]
   urls = load_urls(urls_path)
-  open_tabs(urls)
+  browser_name = sys.argv[2]
+  open_tabs(urls, browser_name)
