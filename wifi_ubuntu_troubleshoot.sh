6888b3f1b863e81521991258651c8863ac4145f0
Alessio Gottardo <tappoz84@yahoo.it>
2020-06-14 15:43:35 +0100

    Fix colors for the wifi troubleshooting script

--- wifi_ubuntu_troubleshoot.sh
+++ wifi_ubuntu_troubleshoot.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+NC="\033[0m"
+RED="\033[1;31m"
+GRN="\033[1;32m"
+YEL="\033[1;33m"
+MAG="\033[1;35m"
+CYN="\033[1;36m"
+
 log() {
-    printf "\n$(date +%Y-%m-%dT%T) ${1}\n\n"
+    printf "\n${RED}$(date +%Y-%m-%dT%T)${NC} ${GRN}${1}${NC}\n\n"
 }
 
 log "Check network interfaces that are DOWN..."
